5fa9a6d9430a192347c701a63a922b48c115aa66
Fletcher <fdurant@ufl.edu>
2016-03-25 10:06:50 -0400

    Decided to keep Pluto and Ceres, for now.

--- planet_list.r
+++ planet_list.r
@@ -1,3 +1,4 @@
 planets = c('mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto', 'ceres')
 print(planets)
 print("But what about Pluto and Ceres?")
+print("Pluto and Ceres are key to understanding our solar system. Leave them for now.")
