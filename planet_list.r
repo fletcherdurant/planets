edb1483dd6ed902581b5f513649ec5994ed9123e
Matthew Collins <mcollins@acis.ufl.edu>
2016-03-25 11:39:48 -0400

    Corrected Pluto's designation

--- planet_list.r
+++ planet_list.r
@@ -2,3 +2,4 @@ planets = c('mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 
 print(planets)
 print("But what about Pluto and Ceres?")
 print("Pluto and Ceres are key to understanding our solar system. Leave them for now.")
+print("Pluto is not a planet. I looked it up on the Internet.")
