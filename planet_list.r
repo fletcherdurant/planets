22fe7c5b73ea65a2eac0cf4164b2a247d5d84c18
Fletcher <fdurant@ufl.edu>
2016-03-25 11:51:20 -0400

    brought in public sourcing

--- planet_list.r
+++ planet_list.r
@@ -3,3 +3,5 @@ print(planets)
 print("But what about Pluto and Ceres?")
 print("Pluto and Ceres are key to understanding our solar system. Leave them for now.")
 print("Pluto is not a planet. I looked it up on the Internet.")
+
+# wikipedia might disagree when I have a free moment to make unsolicited edits.
